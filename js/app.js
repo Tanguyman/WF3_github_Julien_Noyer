1985dc0adec3d770b2a417037a91ce1a41018618
djtanguyparis <dj.tanguy.paris@gmail.com>
2017-07-19 17:33:58 +0200

    Ajout de l'ajax

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    $.ajax({
+
+        type: 'GET',
+        datatype: 'JSON',
+        url: 'data/data.json',
+
+        success: function(data){
+            console.log(data);
+
+            for( var i = 0; i < data.length; i++ ){
+                $('nav ul').append('<li><a href="' + data[i].anchor + '">' + data[i].content + '</a></li>');
+            }
+        },
+
+        error: function(err){
+            console.log(err);
+        },
+
+        complete: function(){
+            console.log('fin de la requète');
+        }
+
+    })
+
+})
